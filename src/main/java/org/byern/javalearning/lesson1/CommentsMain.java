19ba9c35630d8922b75389da9e0555786ae2b79d
ByerN <warhog619@gmail.com>
2017-02-18 14:43:10 +0100

    Lesson 1

---
+++ src/main/java/org/byern/javalearning/lesson1/CommentsMain.java
@@ -0,0 +1,56 @@
+package org.byern.javalearning.lesson1;
+
+/**
+ * Created by ByerN on 18.02.2017.
+ */
+public class CommentsMain {
+
+    /**
+     * This is JavaDoc supported format of comment.
+     * If you use this pattern, JavaDoc will be able to create a documentation.
+     *
+     * If you want to generate javaDoc go to Tools/GenerateJavaDoc (Intellij 2016.2.5)
+     *
+     * @param args this is description of args.
+     *
+     */
+    public static void main(String[] args) {
+        //This is one line comment.
+
+        String text = "SomeText"; //Everything after "//" will be omitted in compilation process
+
+        //If you try
+        //to use
+        //more then one line
+        //of comment -> don't do something like this. It's an ugly way.
+
+        /*
+            Try to use this pattern.
+            Everything between \/* *\/ ('\' sign has been used to escape characters)
+            is not used in compile process.
+        */
+
+//        int a;
+//        a = 1 + 2;
+//        a = a + 3;
+//        CTRL + / combination allows to comment selected lines like this
+
+        /*
+        int a;
+        a = 1 + 2;
+        a = a + 3;
+        CTRL + SHIFT + / combination allows to comment selected lines like this
+        */
+
+        /*
+            Caution! Commented code is a bad practice.
+         */
+
+        /*
+        * This style is also supported.
+        * Intellij will add
+        * new '*' sign on each new line for better readability.
+        *
+        */
+    }
+}
---
+++ src/main/java/org/byern/javalearning/lesson1/PrimitivesMain.java
@@ -0,0 +1,73 @@
+package org.byern.javalearning.lesson1;
+
+/**
+ * Created by ByerN on 18.02.2017.
+ */
+public class PrimitivesMain {
+
+    /**
+     * This is a main method.
+     *
+     * It's acting like an entry point for program.
+     *
+     * This is main method convention:
+     * public static void main(String[] argumentsName)
+     *
+     * args -> arguments passed to program in execution process.
+     *
+     * More about specific keywords later.
+     */
+    public static void main(String[] args){
+
+        //1 byte = 8 bits
+
+        /*
+            Integer variables:
+            byte < short < int < long
+
+            e.g.
+            byte 1B -> can hold value between -128 and 127
+         */
+        //byte variable (1B)
+        byte byteVariable = 0;
+        //short variable (means shorter integer value) (2B)
+        short shortVariable = 0;
+        //int variable (integer value) (4B)
+        int intVariable = 0;
+        //long variable (means longer integer value) (8B)
+        long longVariable = 0L;
+
+        /*
+            Floating point variables:
+            float < double
+         */
+        //float variable (floating point) (4B)
+        float floatVariable = 0.0f;
+        //double variable (double precision floating point) (8B)
+        double doubleVariable = 0.0;
+
+        //char variable (unicode character) (2B)
+        char charVariable = 0;
+
+        /*
+            String is an special type of data type.
+            It's not an primitive type but it's specially supported by Java
+            e.g '+' operator overloaded as string concatenation.
+         */
+        //String variable (string of chars)
+        String stringVariable = "Some string value"; //Default value -> null
+
+        //boolean value (true or false) (1 bit)
+        boolean booleanValue = false;
+
+        System.out.println(byteVariable);
+        System.out.println(shortVariable);
+        System.out.println(intVariable);
+        System.out.println(longVariable);
+        System.out.println(floatVariable);
+        System.out.println(doubleVariable);
+        System.out.println(charVariable);
+        System.out.println(stringVariable);
+        System.out.println(booleanValue);
+    }
+}
