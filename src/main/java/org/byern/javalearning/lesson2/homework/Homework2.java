3a2e003bc4b44dfd838de5a25c18bdda134caebe
ByerN <warhog619@gmail.com>
2017-02-21 17:25:40 +0100

    Lesson2 homework

---
+++ src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
@@ -0,0 +1,36 @@
+package org.byern.javalearning.lesson2.homework;
+
+/**
+ * Created by ByerN on 21.02.2017.
+ */
+public class Homework2 {
+
+    /*
+     * There are two rectangles.
+     *
+     * Properties that describes rectangles are: x, y, w, h
+     * x,y -> rectangle left-bottom corner coordinates
+     * w -> width
+     * h -> height
+     *
+     *
+     * 1. Read properties x1, y1, w1, h1, x2, y2, w2, h2 from input (Scanner usage)
+     * 2. Store it in variables
+     * 3a. If rectangles are colliding with each other (overlapping), print "Collision detected!"
+     * 3b. else -> print "Collision not detected!"
+     *
+     * If the smaller one is in the bigger one it's still collision.
+     * Inclusive collision.
+     *
+     * Advanced:
+     * -Read a boolean flag from input that says if collision check is inclusive or exclusive
+     *  and use it in collision calculation (true -> inclusive, false -> exclusive)
+     *
+     * Hint:
+     * -https://en.wikipedia.org/wiki/De_Morgan%27s_laws
+     * 
+     */
+    public static void main(String[] args) {
+    }
+
+}
