44dc6b9bbf18320845996f70cf1fe7d9ac13bbc9
ByerN <warhog619@gmail.com>
2017-02-22 21:52:04 +0100

    Lesson3 homework

---
+++ src/main/java/org/byern/javalearning/lesson3/homework/Homework3.java
@@ -0,0 +1,75 @@
+package org.byern.javalearning.lesson3.homework;
+
+import java.util.Random;
+
+/**
+ * Created by ByerN on 22.02.2017.
+ */
+public class Homework3 {
+
+    public static final int VIEWPORT_X = 100;
+    public static final int VIEWPORT_Y = 30;
+    public static final double PROBE = 0.001;
+
+    public static void main(String[] args) {
+         /*
+         * Draw random ASCII circle
+         *
+         * -Can be cut on the viewport edges.
+         * -Center has to be in viewport
+         *
+         * Example output:
+         *
+
+                              ++++++
+                             ++    ++
+                             +      +
+                             +      +
+                             +   X  ++
+                             +      +
+                             ++    ++
+                              ++++++
+
+         * Example output:
+                                        +                                +
+                                       ++                                ++
+                                       +                                  +
+                                      ++                                  ++
+                                      +                                    +
+                                     ++                                    ++
+                                     +                                      +
+                                     +                                      +
+                                     +                                      +
+                                     +                                      +
+                                     +                                      +
+                                     +                                      +
+                                     +                   X                  ++
+                                     +                                      +
+                                     +                                      +
+                                     +                                      +
+                                     +                                      +
+                                     +                                      +
+                                     ++                                    ++
+                                      +                                    +
+                                      ++                                  ++
+                                       +                                  +
+                                       ++                                ++
+                                        +                                +
+                                         +                              +
+                                         ++                            ++
+                                          ++                          ++
+                                           ++                        ++
+                                             ++                    ++
+         * X -> center of the circle
+         *
+         *
+         * Hints:
+         * -2d array can be useful.
+         * -Math.cos
+         * -Math.sin
+         * -Math.PI (constant)
+         * -Random.nextInt(n)
+         */
+    }
+
+}
