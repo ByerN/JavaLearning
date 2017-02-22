c0561e192938b1378cb131b1a1b18874a040d7ed
ByerN <warhog619@gmail.com>
2017-02-22 21:52:04 +0100

    Lesson3 homework

---
+++ src/main/java/org/byern/javalearning/lesson3/homework/Homework4.java
@@ -0,0 +1,60 @@
+package org.byern.javalearning.lesson3.homework;
+
+/**
+ * Created by ByerN on 22.02.2017.
+ */
+public class Homework4 {
+
+    public static final int VIEWPORT_X = 100;
+    public static final int VIEWPORT_Y = 30;
+    public static final double PROBE = 0.001;
+
+    public static void main(String[] args) {
+        /*
+         * Draw random ASCII Cubic Bezier curve
+         * https://pl.wikipedia.org/wiki/Krzywa_B%C3%A9ziera
+         *
+         * -Can be cut on the viewport edges.
+         * Example output:
+         *
+
+
+
+
+
+
+                                                                        +++A
+                                                                      +++
+                                                                    +++
+     D++                                                          +++
+       +++                                                       ++
+         +++                                                   ++
+           ++++                                              +++
+              +++                                          +++
+                ++++                                    ++++
+                   ++++                               +B+
+                      +++++                        ++++
+                          +++++                +++++
+                               +++++++++++++++++
+
+
+
+
+
+
+
+
+
+                                             C
+         *
+         * A,B,C,D -> Curve points (optional to render)
+         *
+         * Hints:
+         * -2d array can be useful.
+         * -Math.pow
+         * -Random.nextInt(n)
+         */
+
+    }
+
+}
