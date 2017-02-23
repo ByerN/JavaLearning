c9bb489b78a4ebbac8adf438b37120518f9bb357
wojtek <ronin12@wp.pl>
2017-02-23 08:48:18 +0100

    homework1/ 2

--- src/main/java/org/byern/javalearning/lesson2/homework/Homework1.java
+++ src/main/java/org/byern/javalearning/lesson2/homework/Homework1.java
@@ -1,5 +1,10 @@
 package org.byern.javalearning.lesson2.homework;
 
+//import java.util.Scanner;
+
+import java.util.Scanner;
+
+
 /**
  * Created by ByerN on 21.02.2017.
  */
@@ -27,9 +32,42 @@ public class Homework1 {
      * Hint:
      * -Math library can be useful. Check Math.sqrt(variable) documentation for example.
      * -https://en.wikipedia.org/wiki/Pythagorean_theorem
+     * odleglosc punktow suma promieni
      *
      */
     public static void main(String[] args) {
-    }
 
+        float x1=0,y1=0,r1=0;
+        float x2=0,y2=0,r2=0;
+        double distOfCircleCentres=0;
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me data:");
+        System.out.print("x1: ");
+        x1 = Float.parseFloat(scanner.next());
+        System.out.print("y1: ");
+        y1 = Float.parseFloat(scanner.next());
+        System.out.print("r1:");
+        r1 = Float.parseFloat(scanner.next());
+
+        System.out.println("Give me data:");
+        System.out.print("x2: ");
+        x2 = Float.parseFloat(scanner.next());
+        System.out.print("y2: ");
+        y2 = Float.parseFloat(scanner.next());
+        System.out.print("r2:");
+        r2 = Float.parseFloat(scanner.next());
+
+        distOfCircleCentres = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+
+        if (distOfCircleCentres<r1+r2){
+            System.out.println("Colision detected");
+        }
+        else{
+            System.out.println("Colision not detected");
+        }
+    }
 }
--- src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
+++ src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
@@ -1,5 +1,7 @@
 package org.byern.javalearning.lesson2.homework;
 
+import java.util.Scanner;
+
 /**
  * Created by ByerN on 21.02.2017.
  */
@@ -28,9 +30,54 @@ public class Homework2 {
      *
      * Hint:
      * -https://en.wikipedia.org/wiki/De_Morgan%27s_laws
-     * 
+     *
+     * rzutowanie na osie i true true
+     *
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter x1, y1 w1[width], h1[height]");
+        System.out.print("x1");
+        float x1 = Float.parseFloat(scanner.next());
+        System.out.println();
+        System.out.print("y1");
+        float y1 = Float.parseFloat(scanner.next());
+        System.out.println();
+        System.out.print("w1");
+        float w1 = Float.parseFloat(scanner.next());
+        System.out.println();
+        System.out.print("h1");
+        float h1 = Float.parseFloat(scanner.next());
+
+        System.out.println("Please enter x2, y2 w2[width], h2[height]");
+        System.out.print("x2");
+        float x2 = Float.parseFloat(scanner.next());
+        System.out.println();
+        System.out.print("y2");
+        float y2 = Float.parseFloat(scanner.next());
+        System.out.println();
+        System.out.print("w2");
+        float w2 = Float.parseFloat(scanner.next());
+        System.out.println();
+        System.out.print("h2");
+        float h2 = Float.parseFloat(scanner.next());
+
+        //       x1  <=   x2   <=   x1 + w1
+        //      y1   <=    y2   <= y1+ h1
+
+        if ((((x1<=x2)&&(x2 <= x1+w1)) && ((y1<=y2)&&(y2<=y1+h1)))
+                                ||
+                (((x2<=x1)&&(x1 <= x2+w2)) && ((y2<=y1)&&(y1<=y2+h2)))
+                )
+        {
+            System.out.println("Colision detected!");
+        }
+
+        else
+        {
+            System.out.println("Colision not detected");
+        }
+
     }
 
 }
