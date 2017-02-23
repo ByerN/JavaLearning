45d0d6ba509374f03bde5dcb10ae9f1c7dc60d3b
wojtek <ronin12@wp.pl>
2017-02-23 10:05:34 +0100

    homework

--- src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
+++ src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
@@ -64,7 +64,8 @@ public class Homework2 {
 
         //       x1  <=   x2   <=   x1 + w1
         //      y1   <=    y2   <= y1+ h1
-
+        // operator ternarny ?
+        //
         if ((((x1<=x2)&&(x2 <= x1+w1)) && ((y1<=y2)&&(y2<=y1+h1)))
                                 ||
                 (((x2<=x1)&&(x1 <= x2+w2)) && ((y2<=y1)&&(y1<=y2+h2)))
