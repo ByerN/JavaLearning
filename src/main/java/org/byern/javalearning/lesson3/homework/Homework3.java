b61757f112102ad0c8888c088a939d911944b975
Iwona <iwonajankowsky@gmail.com>
2017-02-26 01:01:29 +0100

    homework 3.3, 3.4

--- src/main/java/org/byern/javalearning/lesson3/homework/Homework3.java
+++ src/main/java/org/byern/javalearning/lesson3/homework/Homework3.java
@@ -2,74 +2,52 @@ package org.byern.javalearning.lesson3.homework;
 
 import java.util.Random;
 
-/**
- * Created by ByerN on 22.02.2017.
- */
 public class Homework3 {
 
     public static final int VIEWPORT_X = 100;
     public static final int VIEWPORT_Y = 30;
-    public static final double PROBE = 0.001;
 
     public static void main(String[] args) {
-         /*
-         * Draw random ASCII circle
-         *
-         * -Can be cut on the viewport edges.
-         * -Center has to be in viewport
-         *
-         * Example output:
-         *
 
-                              ++++++
-                             ++    ++
-                             +      +
-                             +      +
-                             +   X  ++
-                             +      +
-                             ++    ++
-                              ++++++
+        Random rand = new Random();
 
-         * Example output:
-                                        +                                +
-                                       ++                                ++
-                                       +                                  +
-                                      ++                                  ++
-                                      +                                    +
-                                     ++                                    ++
-                                     +                                      +
-                                     +                                      +
-                                     +                                      +
-                                     +                                      +
-                                     +                                      +
-                                     +                                      +
-                                     +                   X                  ++
-                                     +                                      +
-                                     +                                      +
-                                     +                                      +
-                                     +                                      +
-                                     +                                      +
-                                     ++                                    ++
-                                      +                                    +
-                                      ++                                  ++
-                                       +                                  +
-                                       ++                                ++
-                                        +                                +
-                                         +                              +
-                                         ++                            ++
-                                          ++                          ++
-                                           ++                        ++
-                                             ++                    ++
-         * X -> center of the circle
-         *
-         *
-         * Hints:
-         * -2d array can be useful.
-         * -Math.cos
-         * -Math.sin
-         * -Math.PI (constant)
-         * -Random.nextInt(n)
-         */
+        char[][] circle = new char[VIEWPORT_Y][VIEWPORT_X];
+        int x, y, row, column;
+        int centerX = (int)(VIEWPORT_X/2);
+        int centerY = (int)(VIEWPORT_Y/2);
+        int r = rand.nextInt((int)(Math.max(VIEWPORT_X, VIEWPORT_Y)/2));
+        double angle;
+
+        for(row = 0; row < VIEWPORT_Y; row++)
+        {
+            for(column = 0; column < VIEWPORT_X; column++)
+            {
+                circle[row][column] = ' ';
+            }
+        }
+
+        for(int i=0;  i < 360;  i++)
+        {
+            angle = Math.toRadians(i);
+            x = (int)(r * Math.cos(angle)) + centerX;
+            y = (int)(r * Math.sin(angle)) + centerY;
+
+            if(x > -1 && y > -1 && x < VIEWPORT_X && y < VIEWPORT_Y)
+            {
+                circle[y][x] = '+';
+            }
+        }
+
+        circle[centerY][centerX] = 'X';
+
+        for(row = 0; row < VIEWPORT_Y; row++)
+        {
+            for(column = 0; column < VIEWPORT_X; column++)
+            {
+                System.out.print(circle[row][column]);
+            }
+            System.out.println(" ");
+        }
     }
 
 }
--- src/main/java/org/byern/javalearning/lesson3/homework/Homework4.java
+++ src/main/java/org/byern/javalearning/lesson3/homework/Homework4.java
@@ -1,5 +1,7 @@
 package org.byern.javalearning.lesson3.homework;
 
+import java.util.Random;
+
 /**
  * Created by ByerN on 22.02.2017.
  */
@@ -7,53 +9,49 @@ public class Homework4 {
 
     public static final int VIEWPORT_X = 100;
     public static final int VIEWPORT_Y = 30;
-    public static final double PROBE = 0.001;
+    public static final double COUNTER = 100;
 
     public static void main(String[] args) {
-        /*
-         * Draw random ASCII Cubic Bezier curve
-         * https://pl.wikipedia.org/wiki/Krzywa_B%C3%A9ziera
-         *
-         * -Can be cut on the viewport edges.
-         * Example output:
-         *
 
+        Random rand = new Random();
 
+        double t;
+        int SEGMENT_COUNT = 100, row, column, x, y;
+        int[] P1 = {rand.nextInt(VIEWPORT_Y), rand.nextInt(VIEWPORT_X)};
+        int[] P2 = {rand.nextInt(VIEWPORT_Y), rand.nextInt(VIEWPORT_X)};
+        int[] P3 = {rand.nextInt(VIEWPORT_Y), rand.nextInt(VIEWPORT_X)};
+        int[] P4 = {rand.nextInt(VIEWPORT_Y), rand.nextInt(VIEWPORT_X)};
+        char[][] curve = new char[VIEWPORT_Y][VIEWPORT_X];
 
+        for(row = 0; row < VIEWPORT_Y; row++)
+        {
+            for(column = 0; column < VIEWPORT_X; column++)
+            {
+                curve[row][column] = ' ';
+            }
+        }
 
+        for(int i = 0; i <= COUNTER; i++)
+        {
+            t = i / (float) SEGMENT_COUNT;
+            x = (int)(Math.pow(1-t, 3) * P1[0] + 3 * Math.pow(1-t, 2) * t * P2[0] + 3 * Math.pow(1-t, 2) * Math.pow(t, 2) * P3[0] + Math.pow(t, 3) * P4[0]);
+            y = (int)(Math.pow(1-t, 3) * P1[1] + 3 * Math.pow((int)1-t, 2) * t * P2[1] + 3 * Math.pow(1-t, 2) * Math.pow(t, 2) * P3[1] + Math.pow(t, 3) * P4[1]);
+            curve[x][y] = '+';
+        }
 
+        curve[P1[0]][P1[1]] = 'A';
+        curve[P2[0]][P2[1]] = 'B';
+        curve[P3[0]][P3[1]] = 'C';
+        curve[P4[0]][P4[1]] = 'D';
 
-                                                                        +++A
-                                                                      +++
-                                                                    +++
-     D++                                                          +++
-       +++                                                       ++
-         +++                                                   ++
-           ++++                                              +++
-              +++                                          +++
-                ++++                                    ++++
-                   ++++                               +B+
-                      +++++                        ++++
-                          +++++                +++++
-                               +++++++++++++++++
-
-
-
-
-
-
-
-
-
-                                             C
-         *
-         * A,B,C,D -> Curve points (optional to render)
-         *
-         * Hints:
-         * -2d array can be useful.
-         * -Math.pow
-         * -Random.nextInt(n)
-         */
+        for(row = 0; row < VIEWPORT_Y; row++)
+        {
+            for(column = 0; column < VIEWPORT_X; column++)
+            {
+                System.out.print(curve[row][column]);
+            }
+            System.out.println();
+        }
 
     }
 
