4982bb3889e8406023b8db6941766e42b58c77e8
Konrad A <kokosowy@gmail.com>
2017-02-27 23:41:47 +0100

    So far Homework1 to Lesson3

--- src/main/java/org/byern/javalearning/lesson3/homework/Homework1.java
+++ src/main/java/org/byern/javalearning/lesson3/homework/Homework1.java
@@ -3,6 +3,9 @@
 /**
  * Created by ByerN on 22.02.2017.
  */
+ 
+import java.util.Scanner;  
+ 
 public class Homework1 {
 
     public static void main(String[] args) throws InterruptedException {
@@ -11,31 +14,42 @@ public class Homework1 {
          *
          * -play whole animation n-times -> n provided as input
          */
-        for(int n=0; n< 40; n++){
-            if(n % 10 == 0){
-                System.out.println("   x   ");
-            } else if(n % 10 == 1){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 2){
-                System.out.println(" xxxxx ");
-            } else if(n % 10 == 3){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 4){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 5){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 6){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 7){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 8){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 9){
-                System.out.println("  xxx  ");
-            }
-
-            Thread.sleep(300);
-        }
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Provide n: ");
+        int n = input.nextInt();
+        
+        String[] array = {"   x   ", "  xxx  ", " xxxxx ", "  xxx  ", "  xxx  ", "  xxx  ", "  xxx  ", "  xxx  ", "  xxx  ", "  xxx  "};
+        for (int i = 0; i < n; i++) {
+			System.out.println(array[(i % 10)]);
+			Thread.sleep(300);
+		}
+            
+        //for(int n=0; n< 40; n++){
+            //if(n % 10 == 0){
+                //System.out.println("   x   ");
+            //} else if(n % 10 == 1){
+                //System.out.println("  xxx  ");
+            //} else if(n % 10 == 2){
+                //System.out.println(" xxxxx ");
+            //} else if(n % 10 == 3){
+                //System.out.println("  xxx  ");
+            //} else if(n % 10 == 4){
+                //System.out.println("  xxx  ");
+            //} else if(n % 10 == 5){
+                //System.out.println("  xxx  ");
+            //} else if(n % 10 == 6){
+                //System.out.println("  xxx  ");
+            //} else if(n % 10 == 7){
+                //System.out.println("  xxx  ");
+            //} else if(n % 10 == 8){
+                //System.out.println("  xxx  ");
+            //} else if(n % 10 == 9){
+                //System.out.println("  xxx  ");
+            //}
+            //Thread.sleep(300);
+        //}
 
     }
 
