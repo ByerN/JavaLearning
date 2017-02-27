f9726483e3b76c90ad04ac2333975ffd692d1f24
wsobasik <ronin12@wp.pl>
2017-02-27 08:23:51 +0100

    lesson 4 h1

--- src/main/java/org/byern/javalearning/lesson3/homework/Homework1.java
+++ src/main/java/org/byern/javalearning/lesson3/homework/Homework1.java
@@ -11,32 +11,43 @@ public class Homework1 {
          *
          * -play whole animation n-times -> n provided as input
          */
-        for(int n=0; n< 40; n++){
-            if(n % 10 == 0){
-                System.out.println("   x   ");
-            } else if(n % 10 == 1){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 2){
-                System.out.println(" xxxxx ");
-            } else if(n % 10 == 3){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 4){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 5){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 6){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 7){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 8){
-                System.out.println("  xxx  ");
-            } else if(n % 10 == 9){
-                System.out.println("  xxx  ");
+
+
+        String[] strzalka = {"  x  ", " xxx ", "xxxxx", " xxx ", " xxx ", " xxx ", " xxx ", " xxx ", " xxx ", "xxxxx"};
+
+        for (int i = 0; i < 1000; i++) {
+            for (int n = 0; n < 10; n++) {
+                System.out.println(strzalka[n]);
+            }
+            Thread.sleep(300);
+        }
+            for (int n = 0; n < 40; n++) {
+                if (n % 10 == 0) {
+                    System.out.println("   x   ");
+                } else if (n % 10 == 1) {
+                    System.out.println("  xxx  ");
+                } else if (n % 10 == 2) {
+                    System.out.println(" xxxxx ");
+                } else if (n % 10 == 3) {
+                    System.out.println("  xxx  ");
+                } else if (n % 10 == 4) {
+                    System.out.println("  xxx  ");
+                } else if (n % 10 == 5) {
+                    System.out.println("  xxx  ");
+                } else if (n % 10 == 6) {
+                    System.out.println("  xxx  ");
+                } else if (n % 10 == 7) {
+                    System.out.println("  xxx  ");
+                } else if (n % 10 == 8) {
+                    System.out.println("  xxx  ");
+                } else if (n % 10 == 9) {
+                    System.out.println("  xxx  ");
+                }
+
+                Thread.sleep(300);
             }
 
-            Thread.sleep(300);
         }
 
     }
 
-}
