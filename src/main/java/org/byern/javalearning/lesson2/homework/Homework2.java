106c919b4f358be6aa0397bf628041c663061b08
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2017-03-04 21:58:28 +0100

    Lesson 2, Homework2

--- src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
+++ src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
@@ -1,5 +1,7 @@
 package org.byern.javalearning.lesson2.homework;
 
+import java.util.Scanner;
+
 /**
  * Created by ByerN on 21.02.2017.
  */
@@ -31,6 +33,52 @@ public class Homework2 {
      * 
      */
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in); //Creates input to our program
+
+        System.out.println("Enter details of the 1st rectangle (left bottom corner coordinates, width, height)");
+
+        System.out.print("Left bottom corner X1 = ");
+        float x1 = scanner.nextFloat();
+
+        System.out.print("Left bottom corner Y1 = ");
+        float y1 = scanner.nextFloat();
+
+        System.out.print("Width 1 = ");
+        float width1 = scanner.nextFloat();
+
+        System.out.print("Height 1 = ");
+        float height1 = scanner.nextFloat();
+
+        System.out.println();
+
+        System.out.println("Enter details of the 2nd rectangle (left bottom corner coordinates, width, height)");
+
+        System.out.print("Left bottom corner X2 = ");
+        float x2 = scanner.nextFloat();
+
+        System.out.print("Left bottom corner Y2 = ");
+        float y2 = scanner.nextFloat();
+
+        System.out.print("Width 2 = ");
+        float width2 = scanner.nextFloat();
+
+        System.out.print("Height 2 = ");
+        float height2 = scanner.nextFloat();
+
+        System.out.println();
+
+        System.out.println("Result:");
+        System.out.print("The two rectangles collide: " + collisionDetection(x1, y1, width1, height1, x2, y2, width2, height2));
+    }
+
+    public static boolean collisionDetection (float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2){
+
+        if(x1+w1<x2 || x2+w2 < x1 || y1+h1 < y2 || y2+h2 < y1) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
