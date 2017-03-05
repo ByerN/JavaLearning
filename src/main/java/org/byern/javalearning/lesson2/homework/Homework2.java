a9f6cc77ad3a1cc9162f09e2ca413a06f554a2ef
Andrii Stepura <a@w2business.pl>
2017-03-05 09:16:42 +0100

    get homework 5

--- src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
+++ src/main/java/org/byern/javalearning/lesson2/homework/Homework2.java
@@ -1,5 +1,7 @@
 package org.byern.javalearning.lesson2.homework;
 
+import java.util.Scanner;
+
 /**
  * Created by ByerN on 21.02.2017.
  */
@@ -31,6 +33,40 @@ public class Homework2 {
      * 
      */
     public static void main(String[] args) {
-    }
+       Scanner scanner = new Scanner(System.in); //Creates input to our program
+        System.out.println("Enter x1");
+        int x1 = scanner.nextInt();
+        System.out.println("Enter y1");
+        int y1 = scanner.nextInt();
+        System.out.println("Enter w1");
+        int w1 = scanner.nextInt();
+        System.out.println("Enter h1");
+        int h1 = scanner.nextInt();
 
+        System.out.println("Enter x2");
+        int x2 = scanner.nextInt();
+        System.out.println("Enter y2");
+        int y2 = scanner.nextInt();
+        System.out.println("Enter w2");
+        int w2 = scanner.nextInt();
+        System.out.println("Enter h2");
+        int h2 = scanner.nextInt();
+
+
+        boolean checkerx1y1 = (x2<x1&&x1<x2+w2)&&(y2<y1&&y1<y2+h2);
+        boolean checkerx1w1y1 = (x2<x1+w1&&x1+w1<x2+w2)&&(y2<y1&&y1<y2+h2);
+        boolean checkerx1y1h1 = (x2<x1&&x1<x2+w2)&&(y2<y1+h1&&y1+h1<y2+h2);
+        boolean checkerx1w1y1h1 = (x2<x1+w1&&x1+w1<x2+w2)&&(y2<y1+h1&&y1+h1<y2+h2);
+
+        boolean checkerx2y2 = (x1<x2&&x2<x1+w1)&&(y1<y2&&y2<y1+h1);
+        boolean checkerx2w2y2 = (x1<x2+w2&&x2+w2<x1+w1)&&(y1<y2&&y2<y1+h1);
+        boolean checkerx2y2h2 = (x1<x2&&x2<x1+w1)&&(y1<y2+h2&&y2+h2<y1+h1);
+        boolean checkerx2w2y2h2 = (x1<x2+w2&&x2+w2<x1+w1)&&(y1<y2+h2&&y2+h2<y1+h1);
+
+        boolean checker = (checkerx1y1||checkerx1w1y1||checkerx1y1h1||checkerx1w1y1h1||checkerx2y2||checkerx2w2y2||checkerx2y2h2||checkerx2w2y2h2);
+        if (checker==true){System.out.println("Collision detected!");}
+        else {System.out.println("Collision not detected!");}
+
+
+    }
 }
