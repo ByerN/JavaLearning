d39c66979c95d14399796c03cefd310c59ba25c1
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2017-03-06 02:03:11 +0100

    Lesson4, Homework2

--- src/main/java/org/byern/javalearning/lesson4/homework/Homework2.java
+++ src/main/java/org/byern/javalearning/lesson4/homework/Homework2.java
@@ -18,5 +18,52 @@ public class Homework2 {
          *
          * -non-positive numbers can be treated like 0
          */
+        System.out.println("Factorial using recursion:");
+        System.out.println("n = -1, Factorial: " + calculateFactorialUsingRecursion(-1));
+        System.out.println("n = 0, Factorial: " + calculateFactorialUsingRecursion(0));
+        System.out.println("n = 1, Factorial: " + calculateFactorialUsingRecursion(1));
+        System.out.println("n = 2, Factorial: " + calculateFactorialUsingRecursion(2));
+        System.out.println("n = 3, Factorial: " + calculateFactorialUsingRecursion(3));
+        System.out.println("n = 4, Factorial: " + calculateFactorialUsingRecursion(4));
+        System.out.println("n = 5, Factorial: " + calculateFactorialUsingRecursion(5));
+        System.out.println();
+        System.out.println("Factorial using loop:");
+        System.out.println("n = -1, Factorial: " + calculateFactorialUsingLoop(-1));
+        System.out.println("n = 0, Factorial: " + calculateFactorialUsingLoop(0));
+        System.out.println("n = 1, Factorial: " + calculateFactorialUsingLoop(1));
+        System.out.println("n = 2, Factorial: " + calculateFactorialUsingLoop(2));
+        System.out.println("n = 3, Factorial: " + calculateFactorialUsingLoop(3));
+        System.out.println("n = 4, Factorial: " + calculateFactorialUsingLoop(4));
+        System.out.println("n = 5, Factorial: " + calculateFactorialUsingLoop(5));
+
+
+    }
+
+    public static int calculateFactorialUsingRecursion(int n){
+
+        if (n < 0) {
+            return 0;
+        } else if (n == 0){
+            return 1;
+        } else {
+            int factorialResult = n * calculateFactorialUsingRecursion(n-1);
+            n--;
+            return factorialResult;
+        }
+    }
+
+    public static int calculateFactorialUsingLoop(int n){
+
+        if (n < 0) {
+            return 0;
+        } else if (n == 0){
+            return 1;
+        } else {
+            int factorialResult =1;
+            for (int i = 1; i < n+1; i++){
+                factorialResult *= i;
+            }
+            return factorialResult;
+        }
     }
 }
