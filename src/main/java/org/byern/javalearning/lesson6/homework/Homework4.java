f4311704f4fe9b16bf33f4eb5e982c5144ef6efe
ByerN <warhog619@gmail.com>
2017-03-14 09:53:44 +0100

    Homework 6

---
+++ src/main/java/org/byern/javalearning/lesson6/homework/Homework4.java
@@ -0,0 +1,20 @@
+package org.byern.javalearning.lesson6.homework;
+
+/**
+ * Created by krzyspo on 14/03/2017.
+ */
+public class Homework4 {
+
+    public static void main(String[] args) {
+        /*
+         * Array cyclic shift (right).
+         *
+         * public static String[] shiftRight(String[] initialArray, int shiftValue);
+         *
+         * String[] result = shiftRight(new String[]{"a", "b", "c", "d"}, 2);
+         * // {"c", "d", "a", "b"}
+         *
+         */
+    }
+
+}
---
+++ src/main/java/org/byern/javalearning/lesson6/homework/solution/Homework4.java
@@ -0,0 +1,20 @@
+package org.byern.javalearning.lesson6.homework.solution;
+
+/**
+ * Created by krzyspo on 14/03/2017.
+ */
+public class Homework4 {
+
+    public static void main(String[] args) {
+        /*
+         * Array cyclic shift (right).
+         *
+         * public static String[] shiftRight(String[] initialArray, int shiftValue);
+         *
+         * String[] result = shiftRight(new String[]{"a", "b", "c", "d"}, 1);
+         * // {"d", "a", "b", "c"}
+         *
+         */
+    }
+
+}
