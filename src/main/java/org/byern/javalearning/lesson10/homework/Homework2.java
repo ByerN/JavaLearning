55e72157ed3e28be4b42cdc4c1cbdb34e7aee0ae
ByerN <warhog619@gmail.com>
2017-04-11 07:05:18 +0200

    Lesson10 homework

---
+++ src/main/java/org/byern/javalearning/lesson10/homework/Homework2.java
@@ -0,0 +1,20 @@
+package org.byern.javalearning.lesson10.homework;
+
+/**
+ * Created by ByerN on 11.04.2017.
+ */
+public class Homework2 {
+
+    /*
+     * Same as Homework1
+     *
+     * but user should be able to change the comparision order.
+     * It means that programmer should be able to say that only
+     * lastName and age are compared.
+     *
+     * *It can be achieved by adding list with field names (String or better enum) and
+     *  implementing comparision by looping through this array.
+     *
+     */
+
+}
