de7c7f3e63f564ad6905dd9512a63f919157513d
Joanna Sitkowska <asiasit@dublin.ad.s3group.com>
2017-04-24 22:59:12 +0200

    added lombok dependecies AND homework_lesson_10

--- src/main/java/org/byern/javalearning/lesson10/homework/Homework1.java
+++ src/main/java/org/byern/javalearning/lesson10/homework/Homework1.java
@@ -1,5 +1,11 @@
 package org.byern.javalearning.lesson10.homework;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by ByerN on 11.04.2017.
  */
@@ -29,5 +35,52 @@ public class Homework1 {
      * Man should be able to select ascending or descending order. Could be provided as constructor argument
      * in comparator class.
      */
+
+  public static void main(String[] args) {
+
+    List<Person> personList = new ArrayList<>();
+    personList.add(new Person("Thomas", "Moore", 42));
+    personList.add(new Person("Thomas", "Kaminer", 30));
+    personList.add(new Person(null, "Edgar", 58));
+    personList.add(new Person("Julia", "Papineau", 66));
+    personList.add(new Person("David", "Anderson", 87));
+    personList.add(new Person("Robert", "Jeffrey", 14));
+    personList.add(new Person("Mark", "Armstrong", 10));
+    personList.add(new Person("Kelly", "Smith", 60));
+    personList.add(new Person(null, "Smith", 63));
+    personList.add(new Person("Kelly", "Smith", 22));
+    personList.add(new Person("Martin", "Wagner", 35));
+
+
+    //personsList.stream().sorted((p1, p2) -> p1.getFirstName().compareTo(p2.firstName)).collect(Collectors.toList());
+
+//    personsList.sort((p1, p2) -> p1.getFirstName().compareTo(p2.firstName));
+
+    Collections.sort(personList, new ComparatorPerson(true));
+    System.out.println("\nPerson List is sorted ascending order");
+    for(Person person : personList){
+      System.out.println(person);
+    }
+
+    Collections.sort(personList, new ComparatorPerson(false));
+    System.out.println("\nPerson List is sorted descending order");
+    for(Person person : personList){
+      System.out.println(person);
+    }
+
+
+    Collections.sort(personList, new Comparator<Person>() {
+      @Override
+      public int compare(Person o1, Person o2) {
+        return o1.age - o2.age;
+      }
+
+    });
+
+    System.out.println("\nPerson List is sorted by age");
+    for(Person person : personList){
+      System.out.println(person);
+    }
+  }
 
 }
--- src/main/java/org/byern/javalearning/lesson10/homework/Homework2.java
+++ src/main/java/org/byern/javalearning/lesson10/homework/Homework2.java
@@ -1,5 +1,12 @@
 package org.byern.javalearning.lesson10.homework;
 
+
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by ByerN on 11.04.2017.
  */
@@ -14,7 +21,37 @@ public class Homework2 {
      *
      * *It can be achieved by adding list with field names (String or better enum) and
      *  implementing comparision by looping through this array.
-     *
+     *  w konstruktorze lista pól, które mają być sortowane
+     * sortowanie zależne od kolejności na liście
      */
+
+  public static void main(String[] args) {
+
+    List<Person> personList = new ArrayList<>();
+    personList.add(new Person("Thomas", "Moore", 30));
+    personList.add(new Person("Anna", "Kaminer", 30));
+    personList.add(new Person(null, "Edgar", 58));
+    personList.add(new Person("Julia", "Papineau", 66));
+    personList.add(new Person("David", "Anderson", 87));
+    personList.add(new Person("Robert", "Jeffrey", 14));
+    personList.add(new Person("Mark", "Armstrong", 10));
+    personList.add(new Person("Kelly", "Smith", 60));
+    personList.add(new Person(null, "Smith", 63));
+    personList.add(new Person("Kelly", "Smith", 22));
+    personList.add(new Person("Martin", "Wagner", 35));
+
+
+    Collections.sort(personList, new ComparatorPerson_2(true,
+            Arrays.asList(
+                ComparatorPerson_2.Field.AGE,
+                ComparatorPerson_2.Field.FIRST_NAME
+            )
+        )
+    );
+    System.out.println("\nPerson List is sorted ascending order");
+    for (Person person : personList) {
+      System.out.println(person);
+    }
+  }
 
 }
