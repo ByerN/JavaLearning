f2137ff9d95cb07a37b9bca67e54cac1f5125694
wsobasik <ronin12@wp.pl>
2017-05-19 16:35:38 +0200

    hm2les9

--- src/main/java/org/byern/javalearning/lesson9/homework/Homework2.java
+++ src/main/java/org/byern/javalearning/lesson9/homework/Homework2.java
@@ -35,4 +35,46 @@ public class Homework2 {
      * Remember to secure classes (encapsulation)
      */
 
+
+
+
+ /*
+     * Class hierarchy exercise:
+     *
+     * Change following description to class hierarchy:
+
+     * -There is a world
+
+     * -There are humans and beasts in this world
+     * -Humans and beasts have their strength and health point (HP)
+
+     * -Humans can have few roles: Knight, Wizard, Priest
+     * -Knight are strong
+     * -Wizards are casting spells when fighting
+     * -Priests can heal other humans
+
+     * -One human - one role
+     * -Humans can fight beasts
+     * -Humans can be controlled by players
+     * -Humans can have multiple items in their backpack
+     * -Player can attack other player but he will be recognized as player killer (PK)
+
+
+
+     * -Beasts can represent few species: Sheep, Dog and Wolf
+     * -One beast - one specie
+     * -Beasts can fight humans
+     * -Beast cannot attack other beast
+     * -Sheep runs away if they want to attack.
+     * -Dog won't attack if Human has a bone in his backpack. If not- he bites
+     * -Wolf always baits
+     *
+     *
+     *
+     *
+     * Remember to secure classes (encapsulation)
+     */
+
+
+
 }
