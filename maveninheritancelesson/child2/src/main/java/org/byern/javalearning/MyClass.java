ae301adb2e698c5016ce5056262c297d4ced8f42
ByerN <warhog619@gmail.com>
2017-06-06 07:32:55 +0200

    Lesson14

---
+++ maveninheritancelesson/child2/src/main/java/org/byern/javalearning/MyClass.java
@@ -0,0 +1,16 @@
+package org.byern.javalearning;
+
+//import lombok.Getter;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by ByerN on 04.06.2017.
+ */
+//@Getter
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isBlank(""));
+    }
+}
